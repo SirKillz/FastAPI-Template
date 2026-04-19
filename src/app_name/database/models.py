15798b210ce0052144d21b31bfd224880d2b8316
Nick Killeen <sirkillz1198@gmail.com>
2026-04-19 09:39:45 -0500

    Added back mapped json column

--- src/app_name/database/models.py
+++ src/app_name/database/models.py
@@ -32,6 +32,7 @@ class User(Base):
         DateTime(timezone=True),
         default=lambda: datetime.now(UTC)
     )
+    user_metadata: Mapped[dict] = mapped_column(JSON)
 
     # Relationship (one user -> many posts)
     posts: Mapped[list[Post]] = relationship(back_populates="user")
